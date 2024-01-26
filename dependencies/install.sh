9e5dc9330dfe3adb6cb0d622549c1401d9308404
Saroj Raj Sharma <014saroz@gmail.com>
2024-01-26 12:56:10 +0545

    Fix install_apps

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -8,14 +8,15 @@ apt_apps=(curl git zsh terminator xclip qbittorrent)
 snap_apps=(pycharm-community code vlc)
 
 install_apps() {
-    for app in "${2[@]}"; do
-    if ! command -v "$app" &> /dev/null
-    then
-    	echo "$app not found in the system"
-    echo "Installing $app"
-    sudo ${1} install "$app" -y
+  local package_manager=$1
+
+  for app in "${@:2}"; do
+    if ! command -v "$app" &> /dev/null; then
+      echo "$app not found in the system"
+      echo "Installing $app using $package_manager"
+      sudo ${1} install "$app" -y
     fi
-done
+  done
 }
 
 install_apps apt "${apt_apps[@]}"
@@ -42,4 +43,3 @@ sh install.sh
 # install pyenv and pyenv-virtualenv
 curl https://pyenv.run | bash
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-
